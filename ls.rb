cc1269c8dc1c3d49052310914602fc430e717b0a
unikounio <bubbbble428@gmail.com>
2023-04-21 16:33:25 +0900

    とりあえず要件全クリ

--- ls.rb
+++ ls.rb
@@ -1,33 +1,40 @@
 require "debug"
 
-def argument
-  ARGV[0].nil? ? '.' : ARGV[0]
+argument = ARGV[0].nil? ? '.' : ARGV[0]
+
+if File.directory? argument
+  entries = Dir.entries(argument).sort
+elsif File.file? argument
+  puts ARGV[0]
+else
+  puts "ls: \'#{ARGV[0]}\' にアクセスできません: そのようなファイルやディレクトリはありません"
 end
 
-if File.directory?(argument)
-  entries = Dir.entries(argument).sort
-else
-  puts ARGV[0] 
-  exit
-end
+exit unless File.directory? argument
 
 # -aオプションの場合はこの処理を省く必要があるため、entriesとは別の変数として定義している
 entries_normal = entries.reject{|entry|entry.start_with? '.'}
 
-MAX_COLUMN = 3 # 1度しか使わないが最大列数の変更に対応しやすいように定数としている
-columns = entries_normal.each_slice((entries_normal.count.to_f / MAX_COLUMN).ceil).to_a
+public def each_slice_into_rows(max_columns)
+  self.each_slice((self.length.to_f / max_columns).ceil)
+end
+columns = entries_normal.each_slice_into_rows(3).to_a
 
 # 各ブロックの要素数を揃える
 # 足りない要素を' 'で埋める
-columns.each { |column| column.concat([''] * (columns.map(&:size).max - column.size)) }
+columns.each { |column| column.concat([''] * (columns.map(&:length).max - column.length)) }
+
+public def hankaku_ljust(width, padding = ' ')
+  convert_hankaku= 0
+  self.each_char do |char| # エントリの文字ごとに繰り返し処理
+    convert_hankaku += char.bytesize - 2 if char.bytesize > 1
+  end
+  self.ljust(width - convert_hankaku, padding)# ljustの第１引数で扱えるのは全半角を区別しない単純な文字数だが、convert_hankakuを引くことで半角文字数としてカウントできるようにしている
+end
 
 columns.transpose.each do |row| # 行単位で繰り返し処理
   row.each do |entry| # エントリ単位で繰り返し処理
-    moji0 = 0
-    entry.each_char do |char| # エントリの文字ごとに繰り返し処理
-      moji0 += char.bytesize - 2 if char.bytesize > 1
-    end
-    print entry.ljust(30 - moji0, ' ') # 第１引数で扱えるのは全半角を区別しない単純な文字数だが、moji0を引くことで半角文字数としてカウントできるようにしている
+    print entry.hankaku_ljust(18)
   end
   puts
 end
