e7fcfb97204c6c7b10d3bd8e9a5f4ba681791bba
unikounio <bubbbble428@gmail.com>
2023-04-21 14:26:52 +0900

    引数がディレクトリパス以外の場合に対応

--- ls.rb
+++ ls.rb
@@ -1,31 +1,27 @@
 require "debug"
 
-MAX_COLUMN = 3
-DIRECTRY_PATH = ARGV[0].nil? ? '.' : ARGV[0]
-
-# 1.引数で渡されたパスのファイルエントリ名を配列として取得（パスのデフォルト値はカレントディレクトリ）
-# 2.並び変え
-entries = Dir.entries(DIRECTRY_PATH).sort
-
-# '.'と'..'と隠しファイルを消す
-# -aオプションの場合はこの処理を省く
-entries_normal = entries.reject{|entry| entry =~ /^\./}
-
-# 1.列ごとに括って２次元配列にする
-# 2.そのままだとEnumeraterオブジェクトなのでArrayにする
-columns = entries_normal.each_slice((entries_normal.count.to_f / MAX_COLUMN).ceil).to_a # each_sliceの引数が表示するときの行の数になる
-
-# 最大の要素数を求める
-max_size = columns.map(&:size).max
-
-# 各ブロックの要素数を揃える
-columns.each do |column|
-  column.concat([''] * (max_size - column.size)) # 足りない要素を' 'で埋める
+def argument
+  ARGV[0].nil? ? '.' : ARGV[0]
 end
 
-rows = columns.transpose
+if File.directory?(argument)
+  entries = Dir.entries(argument).sort
+else
+  puts ARGV[0] 
+  exit
+end
 
-rows.each do |row| # g行単位で繰り返し処理
+# -aオプションの場合はこの処理を省く必要があるため、entriesとは別の変数として定義している
+entries_normal = entries.reject{|entry|entry.start_with? '.'}
+
+MAX_COLUMN = 3 # 1度しか使わないが最大列数の変更に対応しやすいように定数としている
+columns = entries_normal.each_slice((entries_normal.count.to_f / MAX_COLUMN).ceil).to_a
+
+# 各ブロックの要素数を揃える
+# 足りない要素を' 'で埋める
+columns.each { |column| column.concat([''] * (columns.map(&:size).max - column.size)) }
+
+columns.transpose.each do |row| # 行単位で繰り返し処理
   row.each do |entry| # エントリ単位で繰り返し処理
     moji0 = 0
     entry.each_char do |char| # エントリの文字ごとに繰り返し処理
